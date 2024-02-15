a1454b42d22acd7eebf9b63505622363aa624c75
liyanaa18 <72253489+liyanaa18@users.noreply.github.com>
2024-02-15 21:38:21 +0200

    Add files via upload

---
+++ Linked List/LinkedList Implementations/Implementation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode* next;
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+}
+
+class DoublyLinkedListNode {
+    int data;
+    DoublyLinkedListNode* next;
+    DoublyLinkedListNode* prev;
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+};
+
